41fb47c0331b69c3c30034cb92f38f5ed97d13bf
Avram Stern <avram.stern@gorillalogic.com>
2022-02-08 09:59:05 -0600

    updated year, oops

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <NowPlaying />
         <p className="text-sm text-gray-900 dark:text-gray-100 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2021 —
+          © 2022 —
           <Link href="https://stern9.dev">
             <a
               className="text-gray-900 dark:text-gray-100 ml-1"
